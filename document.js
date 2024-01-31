d57740195d323840900466a8e50703d34f77a783
Giulia Lanari <giulia.lanari96@gmail.com>
2024-01-31 16:43:18 +0100

    U1-S3-L3

---
+++ document.js
@@ -0,0 +1,25 @@
+
+
+const aggiungiAllaTuaLista = document.getElementById("aggiungiAllaTuaLista");
+
+
+
+aggiungiAllaTuaLista.onsubmit = function(e) {
+    aggiungiAllaTuaLista.addEventListener("submit",function (e){
+        
+    })
+}
+
+
+
+
+
+const btrAggiungi = document.getElementById("btrAdd");
+
+const creoUnaFunzioneBtrAggiungi = function () {
+  console.log(btrAggiungi);
+};
+btrAggiungi.onclick = ;
+
+
+
